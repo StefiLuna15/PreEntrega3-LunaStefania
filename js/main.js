0f0a9d6410dd28eb1e6c8dc6771fcb5e2fdbd2a3
Stefanía Luna <stefania.luna15@gmail.com>
2024-04-10 19:02:19 -0300

    Tercera pre entrega

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const btnCambiarTema = document.querySelector("#btnDark");
+const body = document.body;
+const card = document.querySelector("#tarjeta");
+
+btnCambiarTema.addEventListener("click", cambiarTema);
+
+function cambiarTema() {
+    body.classList.toggle("temaOscuro");
+    card.classList.toggle("backCard");
+    if (body.classList.contains("temaOscuro"))
+        btnCambiarTema.innerText = ("Tema Claro");
+    else {
+        btnCambiarTema.innerText = ("Tema Oscuro");
+    }
+}
+
+const formulario = document.querySelector('form');
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nombre= document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const bienvenido = document.querySelector('h2');
+    
+const agregarUsuario = {
+        nombre,
+        email,
+    };
+
+    let usuarios = [];
+
+    if (localStorage.getItem('usuarios')) {
+        usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    }
+
+    usuarios.push(agregarUsuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+    bienvenido.textContent = "Bienvenid@ " + nombre; 
+
+    console.log('Usuario agregado correctamente');
+
+    formulario.reset();
+});
